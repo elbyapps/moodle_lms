815bedab415048a939221aea3c4cd1502346f701
RTB Ops <ops@rtb.gov.rw>
2026-05-05 13:15:59 +0000

    Vendor filter_buku_oembed (no public upstream); ship via Dockerfile COPY

---
+++ vendor/filter_buku_oembed/version.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Filter for component 'filter_buku_oembed'
+ *
+ * @package   filter_buku_oembed
+ * @copyright 2024 BUKU B.V.
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2017101804;
+$plugin->requires = 2016052300;
+$plugin->component = 'filter_buku_oembed';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.0';
